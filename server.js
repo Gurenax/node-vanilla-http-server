b755bdcfca9da1b509838f8f7fd4dbf567fcad4a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-22 16:50:19 +1100

    Add solution to challenges 1 to 7

--- server.js
+++ server.js
@@ -1,8 +1,75 @@
 const HTTP = require('http')
+const { sendJSON, sendHTML, sendCSS, sendGIF } = require('./utils')
+const FS = require('fs')
+// const Path = require('path')
 
-const SERVER = HTTP.createServer((request, response) => {
+// Create the server
+const server = HTTP.createServer((request, response) => {
+  const path = request.url
+  console.log('Request received', path)
+  if (path === '/') {
+    response.end('Home')
+  } else if (path === '/opensesame') {
+    response.end('TOP SECRET!!!')
+  } else if (path === '/postcode.json') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    response.end(`{"name": "Melbourne", "postcode": "3000"}`)
 
+  } else if (path.startsWith('/postcode/')) {
+    postCode = path.replace(new RegExp('/postcode/'),'')
+    FS.readFile(`postcodes/${postCode}`, 'utf8', (err, data) => {
+      if (err) {
+        response.writeHead(404)
+        sendJSON(response, {'error': 'Page not found'})
+      }
+      else {
+        sendJSON(response, data)
+      }
+    });
+    
+  } else if (path === '/about') {
+    sendHTML(response, `
+      <h1>About</h1>
+      <p>This is a paragraph</p>
+      <image src="/assets/example.gif"/>
+      <image src="/assets/example.gif"/>
+      <image src="/assets/example.gif"/>
+      <image src="/assets/example.gif"/>
+    `)
+
+  } else if (path === '/assets/main.css') {
+    FS.readFile('assets/main.css', 'utf8', (err, data) => {
+      if (err) {
+        response.writeHead(404)
+        sendJSON(response, {'error': 'Page not found'})
+      }
+      else {
+        sendCSS(response, data)
+      }
+    });
+
+  } else if (path === '/assets/example.gif') {
+    FS.readFile('assets/example.gif', (err, data) => {
+      if (err) {
+        response.writeHead(404)
+        sendJSON(response, {'error': 'Page not found'})
+      }
+      else {
+        sendGIF(response, data)
+      }
+    });
+
+  } else {
+    response.writeHead(404)
+    sendJSON(response, {'error': 'Page not found'})
+    // response.writeHead(404)
+    // response.end('Page not found')
+  }
 })
 
 // Start the server
-server.listen(7000)
+server.listen(7000, error => {
+  console.log('Server has started at http://localhost:7000')
+})
---
+++ utils.js
@@ -0,0 +1,49 @@
+function sendJSON(response, object) {
+  response.writeHead(response.statusCode, {
+    'Content-Type': 'application/json'
+  })
+  response.end(object)
+}
+
+// function replacer(key, value) {
+//   // Filtering out properties
+//   if (value === '\n') {
+//     return undefined;
+//   }
+//   return value;
+// }
+
+function sendCSS(response, css) {
+  response.writeHead(response.statusCode, {
+    'Content-Type': 'text/css'
+  })
+  response.end(css)
+}
+
+function sendGIF(response, gif) {
+  response.writeHead(response.statusCode, {
+    'Content-Type': 'image/gif'
+  })
+  response.end(gif)
+}
+
+function sendHTML(response, html) {
+  response.writeHead(response.statusCode, {
+    'Content-Type': 'text/html'
+  })
+  // console.log(html)
+  
+  response.end(`
+  <!doctype html>
+  <head>
+  <link rel="stylesheet" type="text/css" href="/assets/main.css">
+  </head>
+  <body>
+  ${html}
+  </body>
+  </html>`)
+}
+      
+module.exports = {
+  sendJSON, sendHTML, sendCSS, sendGIF
+}
